daf94c30e89aeaf201849f3efecb482499a6ef92
garbersb <bengarbers@gmail.com>
2025-11-26 12:56:24 -0600

    Added a SingleMotorSubsystem that will take a Talon motor that the user can spin based on button presses of an XBoxController or Custom Controller

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,7 +9,13 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SelectCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystem.SingleMotorSubsystem;
+
 import java.util.Map;
+
+import com.ctre.phoenix6.hardware.TalonFX;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,7 +24,10 @@ import java.util.Map;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
+
+
   // The enum used as keys for selecting the command to run.
+  /** AUTO GENERATED */
   private enum CommandSelector {
     ONE,
     TWO,
@@ -27,6 +36,7 @@ public class RobotContainer {
 
   // An example selector method for the selectcommand.  Returns the selector that will select
   // which command to run.  Can base this choice on logical conditions evaluated at runtime.
+    /** AUTO GENERATED */
   private CommandSelector select() {
     return CommandSelector.ONE;
   }
@@ -35,6 +45,7 @@ public class RobotContainer {
   // by the selector method at runtime.  Note that selectcommand works on Object(), so the
   // selector does not have to be an enum; it could be any desired type (string, integer,
   // boolean, double...)
+    /** AUTO GENERATED */
   private final Command m_exampleSelectCommand =
       new SelectCommand<>(
           // Maps selector values to commands
@@ -44,7 +55,31 @@ public class RobotContainer {
               Map.entry(CommandSelector.THREE, new PrintCommand("Command three was selected!"))),
           this::select);
 
+
+  // The robot's subsystems 
+  private SingleMotorSubsystem singleMotorSubsystem = null;
+
+
+  // XBoxController Object that points to port 1 of the USB device.  You can use any of the buttons on the XBoxController
+  private final CommandXboxController m_driverController =  new CommandXboxController(1);
+
+   // Generic object that points to our custom button box which is plugged into Port 0
+   // Note that you can see this within the Driver Station Controller
+  private CommandGenericHID comm = new CommandGenericHID(0);
+
+  //Talon Motor object we will look to manipulate.
+ private TalonFX motor = null;
+
+
+  /** RobotContainer that calls a method that configures the buttons */
   public RobotContainer() {
+
+     //We will create our motor object that will be used to spin.
+     motor = new TalonFX(1);
+
+     //SingleMotorSubsystem Object that will be used to turn the motor based on button presses
+     singleMotorSubsystem = new SingleMotorSubsystem(motor);
+
     // Configure the button bindings
     configureButtonBindings();
   }
@@ -55,7 +90,27 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then calling passing it to a
    * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    System.out.println("configureBindings");
+
+    //The Y Button when pressed will spin the motor forward
+    m_driverController.y().onTrue(singleMotorSubsystem.spinArmCommandForward());
+
+    //The Y command will stop when it is false
+    m_driverController.y().onFalse(singleMotorSubsystem.stopArmCommand());
+
+    //The A button will spin the motor backward
+    m_driverController.a().onTrue(singleMotorSubsystem.spinArmCommandBackward());
+
+    //The B button will stop the motor
+    m_driverController.b().onTrue(singleMotorSubsystem.stopArmCommand());
+
+    //We waned to test out our generic controller.  If the first button is presseed we will spin the motor forward
+    //and when it is released it will stop.
+    comm.button(1).onTrue(singleMotorSubsystem.spinArmCommandForward());
+    comm.button(1).onFalse(singleMotorSubsystem.stopArmCommand());
+    
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
---
+++ src/main/java/frc/robot/subsystem/SingleMotorSubsystem.java
@@ -0,0 +1,159 @@
+package frc.robot.subsystem;
+
+import com.ctre.phoenix6.Orchestra;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.PositionDutyCycle;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SingleMotorSubsystem extends SubsystemBase {
+
+    // This is the only motor we are concerned about spinning in this example.
+    TalonFX motor = null;
+
+    // We will use this to play music so we know where things are at within our
+    // code.
+    Orchestra orch = null;
+
+    // DutyCycle values we can set the motor to to spin forward
+    private final DutyCycleOut spinOutputForward = new DutyCycleOut(0.05); // 50% power
+
+    // DutyCycle we can set the motor to stop
+    private final DutyCycleOut stopOutput = new DutyCycleOut(0.0); // Stop motor
+
+    // DutyCycle we can set the motor to go backwards
+    private final DutyCycleOut spinOutputBackward = new DutyCycleOut(-0.05); // 50% power
+
+    // Move to 4 rotations forward
+    PositionDutyCycle rotate4 = new PositionDutyCycle(4.0); // 4 rotations
+
+    String[] _songs = new String[] {
+            "CantinaBand.chrp", "song1.chrp",
+            "StarWarsMainSong.chrp" };
+
+    /* track which song is selected for play */
+    int _songSelection = 0;
+
+    /* overlapped actions */
+    int _timeToPlayLoops = 0;
+
+    /** Creates the SingleMotorSubsystem which takes in a TalonFX motor object.. */
+    public SingleMotorSubsystem(TalonFX motor) {
+        this.motor = motor;
+
+        //This will ensure the motor breaks
+        motor.setNeutralMode(NeutralModeValue.Brake);
+
+        //Here we load the Orchestra object and add the motor object and load the the music
+        orch = new Orchestra();
+        orch.addInstrument(motor);
+        LoadMusicSelection(0);
+
+    }
+
+
+       //* Method that returns a command to spin the motor forward */
+       public Command spinArmCommandForward() {
+        System.out.println("spinArmCommandForward");
+
+        return run(() -> motor.setControl(spinOutputForward))
+            .finallyDo(() -> motor.setControl(stopOutput)); // stop when finished
+      }
+
+
+         /** Method that returns a command to stop the motor */
+        public Command stopArmCommand() {
+            System.out.println("stopArmCommand");                
+            return run(() -> motor.setControl(stopOutput)).finallyDo(() -> motor.setControl(stopOutput)); // stop .gwhen finished
+         }
+
+
+        /** Method that returns a command to spin the motor backward */
+        public Command spinArmCommandBackward() {
+            System.out.println("spinArmCommandBack");
+
+            return run(() -> motor.setControl(spinOutputBackward))
+                        .finallyDo(() -> motor.setControl(stopOutput)); // stop when finished
+        }
+
+
+    /** This method will goForward for a number of rotations that are inputted */
+    public Command goForward(int rotations) {
+        System.out.println("go forward");
+        return this.startEnd(() -> motor.setControl(spinOutputForward), () -> motor.setControl(stopOutput));
+    }
+
+    /** This method will goBackwards for a number of roations that are inputted */
+    public Command goBackward(int rotations) {
+        System.out.println("go Backward");
+        return this.startEnd(() -> motor.setControl(spinOutputBackward), () -> motor.setControl(stopOutput));
+    }
+
+
+    /** This method will top the motor. */
+    public Command stopMotor() {
+        System.out.println("Stop Motor");
+
+        return runOnce(
+                () -> {
+                    motor.setControl(stopOutput);
+                });
+    }
+
+
+    /** goForwardForTme is a method that you can put a time increment in which 
+     * will have the motor spin forwards for a time duration in seconds.
+     */
+    public Command goForwardForTime(double time) {
+
+        System.out.println("go foward for time");
+        return this.startEnd(() -> motor.setControl(spinOutputForward), () -> motor.setControl(stopOutput))
+                .withTimeout(time);
+
+    }
+
+    /** goBackwardForTime is a method that you can put a time increment in which 
+     * will hae the motor spin backwards for a time duration in seconds.
+     */
+    public Command goBackwardForTime(double time) {
+
+        System.out.println("go foward for time");
+        return this.startEnd(() -> motor.setControl(spinOutputBackward), () -> motor.setControl(stopOutput))
+                .withTimeout(time);
+
+    }
+
+    
+    /** LoadMusicSelection takes in a number to load the file to play */
+    public void LoadMusicSelection(int offset) {
+
+        /* increment song selection */
+        _songSelection += offset;
+
+
+        /* wrap song index in case it exceeds boundary */
+        if (_songSelection >= _songs.length) {
+            _songSelection = 0;
+        }
+        if (_songSelection < 0) {
+            _songSelection = _songs.length - 1;
+        }
+        /* load the music files */
+        orch.loadMusic(_songs[_songSelection]);
+
+        /* print to console which file we are playing */
+        System.out.println("Song selected is: " + _songs[_songSelection] + ".  Press left/right on d-pad to change.");
+
+        /*
+         * schedule a play request, after a delay.
+         * This gives the Orchestra service time to parse the music file.
+         * If play() is called immedietely after, you may get an invalid action error
+         * code.
+         */
+        _timeToPlayLoops = 10;
+    }
+
+}
